2811fb272c25f11339c5425e1846fa540975c041
Anshu Sinha <anshujuly2@gmail.com>
2023-03-05 15:45:31 +0530

    Add 'class and objects' codes

---
+++ main.py
@@ -0,0 +1,14 @@
+## Class and objects
+"""creating class"""
+class Computer:
+    """method"""
+    def config(self):
+        print("i3, 8gb ram, 256gb ssd")
+
+
+"""creating object"""
+comp1 = Computer()
+"""print type of comp1"""
+print(type(comp1))
+"""assigning method to objects"""
+comp1.config()
