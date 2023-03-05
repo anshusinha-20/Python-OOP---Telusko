1792655d3d9fa447c52aa9707c0a70d879f7fc89
Anshu Sinha <anshujuly2@gmail.com>
2023-03-05 16:22:37 +0530

    Add 'constructor, self and comparing objects' codes

--- main.py
+++ main.py
@@ -40,3 +40,41 @@
 # """assigning method to objects"""
 # comp1.config()
 # comp2.config()
+
+#####
+
+## Constructor, self and comparing objects
+# """creating computer class"""
+# class Computer:
+
+#     """init method"""
+#     def __init__(self):
+#         self.name = "Anshu"
+#         self.age = 19
+
+#     """update method"""
+#     def update(self):
+#         self.name = "Sahil"
+#         self.age = 17
+
+#     """compare method"""
+#     def compare(self, other): # here self = c1 and other = c2
+#         if (self.age > other.age):
+#             print(f"{self.name} is older than {other.name} by {self.age - other.age} years")
+#         else:
+#             print(f"{other.name} is older than {self.name} by {other.age - self.age} years")
+
+
+# """creating objects"""
+# c1 = Computer()
+# c2 = Computer()
+
+# """changing the attibute values"""
+# # c2.name = "Sahil"
+# # c2.age = 17
+
+# """updates the attributes of the c2 object by calling the update method"""
+# c2.update()
+
+# """comparing ages by calling the compare method"""
+# c1.compare(c2)
