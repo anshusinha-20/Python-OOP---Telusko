882f3013fb2b4eb780f6883218f08ee41686b013
Anshu Sinha <anshujuly2@gmail.com>
2023-03-05 17:01:02 +0530

    Add 'types of variables' codes

--- main.py
+++ main.py
@@ -78,3 +78,39 @@
 
 # """comparing ages by calling the compare method"""
 # c1.compare(c2)
+
+#####
+
+## Types of variables
+# """creating car class"""
+# class Car:
+
+#     """creating static/class variables"""
+#     wheels = 4
+
+#     """creating init method"""
+#     def __init__(self):
+#         self.mileage = 10
+#         self.brand = "Tesla"
+
+
+# """creating objects"""
+# c1 = Car()
+# c2 = Car()
+
+# c2.mileage = 8
+# c2.brand = "Kia"
+
+# print(c1.brand, c1.mileage, c1.wheels)
+# print(c2.brand, c2.mileage, c2.wheels)
+
+# """trying to change the class variable"""
+# c1.wheels = 3
+# print(f"{c1.brand} should have {c1.wheels} wheels!") # it works
+
+# """changing the class variable for all the objects"""
+# Car.wheels = 5
+
+# """printing the update value"""
+# print(c1.brand, c1.mileage, c1.wheels)
+# print(c2.brand, c2.mileage, c2.wheels)
